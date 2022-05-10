5b5ee3d87c132f3ff8399dc959428e5d0f31b97d
3ndless-git <poudelnischal.np@gmail.com>
2022-05-10 22:41:07 +0545

    Remove unnecessary files, Created folder structure

--- public/README.md
+++
@@ -1,6 +0,0 @@
-# Merak
-
-
-Run :
-
-### `npm start`
--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Inventory from "./Pages/Dashboard/Inventory";
 
-function App() {
+import Home from "./Pages/Home/Home";
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+   <Switch>
+     <Route path="/" exact component={Home}/>
+     <Route path="/inventory" exact component={Inventory}/>
+     <Route exact path='*'component={Home}/> 
+   </Switch>
+   </Router>
   );
 }
 
