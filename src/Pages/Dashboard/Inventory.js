e7f1960fd80c165cbb9e0176f91e1ae999f76100
Manash-sth <manash.81142615@gmail.com>
2022-05-11 16:55:15 +0545

    inventory cards....

--- src/Pages/Dashboard/Inventory.js
+++ src/Pages/Dashboard/Inventory.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import styledComponents from 'styled-components'
+import Card from '../../Components/Organisms/Card/Card'
+
+const data = [
+  {
+    id: 1,
+    img: "https://imgs.search.brave.com/M3I0djBm_PCYQ44FDVmtJEai1am2wNJu6C6dDj72ODc/rs:fit:1200:1200:1/g:ce/aHR0cDovL2Nkbi5w/aW5jaG9meXVtLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvTmlj/ZS1DcmVhbS02Lmpw/Zw",
+    title: "Icecream",
+    quantity: "12"
+  },
+  {
+    id: 2,
+    img: "https://imgs.search.brave.com/M3I0djBm_PCYQ44FDVmtJEai1am2wNJu6C6dDj72ODc/rs:fit:1200:1200:1/g:ce/aHR0cDovL2Nkbi5w/aW5jaG9meXVtLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvTmlj/ZS1DcmVhbS02Lmpw/Zw",
+    title: "Icecream",
+    quantity: "12"
+  },
+]
+
+const CardContainer = styledComponents.div`
+  display: flex;
+  gap: 2rem;
+`
 
 const Inventory = () => {
   return (
     <div>
         <h1>Inventory</h1>
+
+        <CardContainer>
+          {
+            data.map((item, index) => (
+              <Card img_src={item.img} title={item.title} quantity={item.quantity} key={index}/>
+            ))
+          }
+        </CardContainer>
+
     </div>
   )
 }
