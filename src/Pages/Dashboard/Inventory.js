d529c46c7b9d0e8e4bbe08679e70b79eeab62afa
Manash-sth <manash.81142615@gmail.com>
2022-05-12 18:29:43 +0545

    storage stats.....

--- src/Pages/Dashboard/Inventory.js
+++ src/Pages/Dashboard/Inventory.js
@@ -32,6 +32,8 @@ import {Search} from '@mui/icons-material'
 import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
+import Stack from '@mui/material/Stack';
+
 
 //Component - Atoms
 import Title from '../../Components/Atoms/Title';
@@ -68,9 +70,29 @@ const packed = [
 ]
 
 
+const stock = [
+  {
+    id: 1,
+    img: "https://imgs.search.brave.com/M3I0djBm_PCYQ44FDVmtJEai1am2wNJu6C6dDj72ODc/rs:fit:1200:1200:1/g:ce/aHR0cDovL2Nkbi5w/aW5jaG9meXVtLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvTmlj/ZS1DcmVhbS02Lmpw/Zw",
+    title: "Icecream",
+    quantity: "12"
+  },
+  {
+    id: 2,
+    img: "https://shoppinglife.lk/wp-content/uploads/2021/11/olive-oil-1-922x1024.png",
+    title: "Extra virgin olive oil",
+    quantity: "10"
+  },
+]
+
+
 const Topbar = styledComponents.div`
   display: flex;
   justify-content: space-between;
+`
+
+const Qu = styledComponents.h1`
+  text-align: center;
 `
 
 const TxtField = styled(TextField)({
@@ -97,7 +119,7 @@ const Inventory = () => {
     <>
       <Container style={{marginTop: '30px'}}>
         <Topbar>
-          <Title title="Inventoryy" />  
+          <Title title="Inventory" />  
           <TxtField 
             id="outlined-basic" 
             label="Search Item" 
@@ -114,6 +136,26 @@ const Inventory = () => {
             }
           />
         </Topbar>
+
+        <Subtitle title="Storage Stats" /> 
+
+        <Stack direction="row" spacing={20}>
+        <Stack spacing={2}>
+          <Qu>{packed.length}</Qu>
+          <span style={{color: '#00A7E3'}}>To be packed</span>
+        </Stack>
+
+        <Stack spacing={2}>
+          <Qu>{delivered.length}</Qu>
+          <span style={{color: '#00A7E3'}}>To be delivered</span>
+        </Stack>
+
+        <Stack spacing={2}>
+          <Qu>{stock.length}</Qu>
+          <span style={{color: '#00A7E3'}}>Total in stock</span>
+        </Stack>
+
+        </Stack> 
 
         <Subtitle title="To be Delivered" />  
        
