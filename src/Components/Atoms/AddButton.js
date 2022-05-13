eec0a5aab55f7cb4f3c2e5bdaea5ea4fed356fe0
Manash-sth <manash.81142615@gmail.com>
2022-05-13 12:02:56 +0545

    spacing and button position

--- src/Components/Atoms/AddButton.js
+++ src/Components/Atoms/AddButton.js
@@ -2,15 +2,8 @@ import * as React from 'react';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 
-const fabStyle = {
-    position: 'absolute',
-    bottom: 16,
-    right: 16,
-  };
-  
-
 const Add = () => (
-    <Fab color="primary" aria-label="add" sx={{fabStyle}}>
+    <Fab sx={{position: 'absolute', bottom: 16, right: 16}}>
         <AddIcon />
     </Fab>
 )
