f4727961881b56e43452500b2935b4b4b5f7c063
3ndless-git <poudelnischal.np@gmail.com>
2022-05-16 16:58:18 +0545

    Minor style changes and Api connect

--- src/Components/Molecules/Card.js
+++ src/Components/Molecules/Card.js
@@ -1,11 +1,18 @@
-//Data match garna use gareko not necessary to use
-import PropTypes from 'prop-types';
+
+import React, {useState} from 'react';
 
 import { Link as RouterLink } from 'react-router-dom';
 
 // Material Ui Components
 import { Box, Link, Card as Cards, Typography, Stack } from '@mui/material';
 import { styled } from '@mui/material/styles';
+
+// Material Ui
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import ProductDetails from './ProductDetails';
 
 
 // -----------Styling Product Image---------
@@ -24,19 +31,33 @@ const ProductImgStyle = styled('img')({
 
 const Card = ({ product }) => {
 
-  const { title, img, quantity} = product;
+  
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  const { name, quantity} = product;
+  const image = product.variant[0].image;
 
   return (
-    <Cards sx={{border: "none", boxShadow: "none", outline: 'none' }} style={{cursor: 'pointer'}} >
+    <>
+    <Cards sx={{border: "none", boxShadow: "none", outline: 'none' }} style={{cursor: 'pointer'}} onClick={handleClickOpen}>
       
       <Box sx={{ pt: '100%', position: 'relative'}}>
-        <ProductImgStyle alt={title} src={img} />
+        <ProductImgStyle alt={name} src={image} />
       </Box>
 
       <Stack spacing={2} sx={{ p: 1}} style={{background: '#181818'}}>
         <Link to="#" color="inherit" underline="hover" component={RouterLink}>
           <Typography variant="subtitle1" style={{color:'gray'}} noWrap>
-            {title}
+            {name}
             <br></br>
             <span style={{color: '#00A7E3'}}>{quantity} orders</span>
           </Typography>
@@ -44,13 +65,30 @@ const Card = ({ product }) => {
       </Stack>
       
     </Cards>
+
+
+    
+    
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title" style={{background: '#181818', color: 'gray'}}>
+          {"Product Details"}
+        </DialogTitle>
+        <DialogContent style={{background: '#181818', color: 'gray'}}>
+          <DialogContentText id="alert-dialog-description">
+            <ProductDetails product={product}/>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    
+    </>
   );
 }
 
-//Not necessary - ignore
-Card.propTypes = {
-  product: PropTypes.object,
-};
 
 
 export default Card;
