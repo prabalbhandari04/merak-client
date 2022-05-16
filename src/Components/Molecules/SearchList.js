5cc7610db50eb5545373e63e900038aaacc771e6
Prabal Bhandari <prabalb046348@gmail.com>
2022-05-16 19:34:25 +0545

    wd

---
+++ src/Components/Molecules/SearchList.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Card from './Card';
+
+function SearchList({ filteredProduct }) {
+  const filtered = filteredProduct.map(product =>  <Card key={product.id} person={product} />); 
+  return (
+    <div>
+      {filtered}
+    </div>
+  );
+}
+
+export default SearchList;
