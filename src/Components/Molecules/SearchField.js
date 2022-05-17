1870055d1e38a6a97c91a45af59bcf38c89d6ffd
Prabal Bhandari <prabalb046348@gmail.com>
2022-05-17 07:10:34 +0545

    Update SearchField.js

--- src/Components/Molecules/SearchField.js
+++ src/Components/Molecules/SearchField.js
@@ -4,6 +4,9 @@ import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
 // import OutlinedInput from '@mui/material/OutlinedInput';
+  import Scroll from './Scroll';
+  import SearchList from './SearchList';
+import {useState} from 'react';
 
 const TxtField = styled(TextField)({
     '& .MuiOutlinedInput-root': {
@@ -22,7 +25,36 @@ const TxtField = styled(TextField)({
   });
   
 
-const SearchField = () => {
+const SearchField = ({details}) => {
+  const [searchField, setSearchField] = useState("");
+    const filteredProduct = details.filter(
+      product => {
+        return (
+          product
+          .title
+          .toLowerCase()
+          .includes(searchField.toLowerCase()) ||
+          product
+          .quantity
+          .toLowerCase()
+          .includes(searchField.toLowerCase())
+        );
+      }
+    );
+  
+    const handleChange = e => {
+      setSearchField(e.target.value);
+    };
+  
+    function searchList() {
+      return (
+        <Scroll>
+          <SearchList filteredProdut={filteredProduct} />
+        </Scroll>
+      );
+    }
+
+
     return (
         <TxtField 
         id="outlined-basic" 
@@ -44,6 +76,22 @@ const SearchField = () => {
   
   export default SearchField;
 
-
-
+  
+  //   return (
+  //     <section className="garamond">
+        
+  //       <div className="pa2">
+  //         <input 
+  //           className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+  //           type = "search" 
+  //           placeholder = "Search People" 
+  //           onChange = {handleChange}
+  //         />
+  //       </div>
+  //       {searchList()}
+  //     </section>
+  //   );
+  // }
+  
+  // export default Search;
   
