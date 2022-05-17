7f8470cc2fae04fee5f313124599c3cbd43b0f0e
abhay <abhayjajodia86@gmial.com>
2022-05-17 21:47:05 +0545

    index.js added orderReducer

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
+import { orderReducer } from './orderReducer';
 import { productReducer } from './productReducer';
 
 const reducers = combineReducers({
     allProducts: productReducer,
+    allOrders: orderReducer
 })
 
 export default reducers;
