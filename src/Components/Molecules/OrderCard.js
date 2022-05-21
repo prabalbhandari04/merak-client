a8344a17d7b97369e5ffd0a9e8856110e18c7be9
abhay <abhayjajodia86@gmial.com>
2022-05-21 11:39:42 +0545

    order card added and order detail added

---
+++ src/Components/Molecules/OrderCard.js
@@ -0,0 +1,90 @@
+
+import React, {useState} from 'react';
+
+import { Link as RouterLink } from 'react-router-dom';
+
+// Material Ui Components
+import { Box, Link, Card as Cards, Typography, Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+// Material Ui
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import OrderDetails from './OrderDetails';
+
+
+// -----------Styling Product Image---------
+
+// const ProductImgStyle = styled('img')({
+//   top: 0,
+//   width: '100%',
+//   height: '100%',
+//   objectFit: 'cover',
+//   position: 'absolute',
+// });
+
+// ------------------------
+
+
+
+const Card = ({ product }) => {
+
+  
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+//   const { name, quantity} = product;
+//   const image = product.variant[0].image;
+
+  return (
+    <>
+    <Cards sx={{border: "none", boxShadow: "none", outline: 'none' }} style={{cursor: 'pointer'}} onClick={handleClickOpen}>
+      
+      <Box sx={{ pt: '100%', position: 'relative'}}>
+        {/* <ProductImgStyle alt={name} src={image} /> */}
+      </Box>
+
+      <Stack spacing={2} sx={{ p: 1}} style={{background: '#181818'}}>
+        <Link to="#" color="inherit" underline="hover" component={RouterLink}>
+          <Typography variant="subtitle1" style={{color:'gray'}} noWrap>
+            {name}
+            <br></br>
+            <span style={{color: '#00A7E3'}}>{quantity} orders</span>
+          </Typography>
+        </Link>
+      </Stack>
+      
+    </Cards>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title" style={{background: '#181818', color: 'gray'}}>
+          {"Product Details"}
+        </DialogTitle>
+        <DialogContent style={{background: '#181818', color: 'gray'}}>
+          <DialogContentText id="alert-dialog-description">
+            <OrderDetails product={product}/>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    
+    </>
+  );
+}
+
+
+
+export default Card;
