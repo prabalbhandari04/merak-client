6e61f34491cea11b27d7097a353446d6df3d0831
3ndless-git <poudelnischal.np@gmail.com>
2022-05-21 14:13:08 +0545

    Do not merge into this branch without creating any Pull requests

--- src/Components/Molecules/Card.js
+++ src/Components/Molecules/Card.js
@@ -1,11 +1,18 @@
-//Data match garna use gareko not necessary to use
-import PropTypes from 'prop-types';
+
+import React, {useState} from 'react';
 
 import { Link as RouterLink } from 'react-router-dom';
 
 // Material Ui Components
 import { Box, Link, Card as Cards, Typography, Stack } from '@mui/material';
 import { styled } from '@mui/material/styles';
+
+// Material Ui
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import ProductDetails from './ProductDetails';
 
 
 // -----------Styling Product Image---------
@@ -24,33 +31,62 @@ const ProductImgStyle = styled('img')({
 
 const Card = ({ product }) => {
 
-  const { title, img, quantity} = product;
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+
 
   return (
-    <Cards sx={{border: "none", boxShadow: "none", outline: 'none' }}>
+    <>
+    <Cards sx={{border: "none", boxShadow: "none", outline: 'none' }} style={{cursor: 'pointer'}} onClick={handleClickOpen}>
       
       <Box sx={{ pt: '100%', position: 'relative'}}>
-        <ProductImgStyle alt={title} src={img} />
+        <ProductImgStyle alt={product.name} src={`https://merak-test.herokuapp.com${product.default_image}`}/>
       </Box>
 
       <Stack spacing={2} sx={{ p: 1}} style={{background: '#181818'}}>
         <Link to="#" color="inherit" underline="hover" component={RouterLink}>
           <Typography variant="subtitle1" style={{color:'gray'}} noWrap>
-            {title}
+            {product.name}
             <br></br>
-            <span style={{color: '#00A7E3'}}>{quantity} orders</span>
+            <span style={{color: '#00A7E3'}}>{product.quantity} orders</span>
           </Typography>
         </Link>
       </Stack>
       
     </Cards>
+
+
+    
+    
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title" style={{background: '#181818', color: 'gray'}}>
+          {"Product Details"}
+        </DialogTitle>
+        <DialogContent style={{background: '#181818', color: 'gray'}}>
+          <DialogContentText id="alert-dialog-description" component="div">
+            <ProductDetails product={product}/>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    
+    </>
   );
 }
 
-//Not necessary - ignore
-Card.propTypes = {
-  product: PropTypes.object,
-};
 
 
 export default Card;
---
+++ src/Components/Organisms/Stats.js
@@ -0,0 +1,46 @@
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {loadProducts} from '../../Redux/Actions/productsActions';
+import styledComponents from 'styled-components' //Styled components
+import Stack from '@mui/material/Stack'; //Material Ui
+
+
+const Qu = styledComponents.h1`
+text-align: center;
+font-size: 3rem;
+font-weight: 900;
+margin: .5rem;
+`
+
+const Stats = () => {
+
+  const dispatch = useDispatch();
+  const {products} = useSelector(state => state.data);
+
+  useEffect(() => {
+    dispatch(loadProducts());
+  }, [dispatch]);
+ 
+
+  return (
+    <Stack direction="row" spacing='5rem'>
+    <Stack>
+      <Qu>{products.length}</Qu>
+      <span style={{color: '#00A7E3'}}>To be packed</span>
+    </Stack>
+
+    <Stack>
+      <Qu>{products.length}</Qu>
+      <span style={{color: '#00A7E3'}}>To be delivered</span>
+    </Stack>
+
+    <Stack>
+      <Qu>{products.length}</Qu>
+      <span style={{color: '#00A7E3'}}>Total in stock</span>
+    </Stack>
+
+    </Stack> 
+  );
+}
+
+export default Stats
