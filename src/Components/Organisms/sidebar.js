03b8ddc4ffd9acc2c728cff4d6dd7c0a44541620
abhay <abhayjajodia86@gmial.com>
2022-06-15 09:49:49 +0545

    navbar position update in homr page

--- src/Components/Organisms/sidebar.js
+++ src/Components/Organisms/sidebar.js
@@ -11,7 +11,42 @@ import { MdPersonAddAlt1 } from 'react-icons/md'
 import { RiGalleryFill } from 'react-icons/ri'
 
 const Container = styledComponents.nav`
-    width: ${props => props.width}
+    width: ${props => props.width};
+    position: relative;
+    background-color: black;
+    height: 100vh;
+    transition : all 0.3s ease-in-out;
+    z-index: 40;
+
+`
+const Icon = styledComponents.div`
+    position: absolute;
+    right: -2rem;
+    top: 2rem;
+    width:3rem;
+    height:3rem;
+    background-color: black;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+`
+const Wrapper = styledComponents.div`
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    padding:0 1rem;
+    width: 100%;
+    row-gap:2rem;
+    align-items: ${props => props.align};
+
+    div{
+        display: flex;
+        column-gap: 1rem;
+        cursor: pointer;
+        color: ${props => props.color};
+    }
 `
 
 const Navbar = () => {
@@ -29,20 +64,20 @@ const Navbar = () => {
 
     const getJustify = () => {
         if(!isOpen) {
-            return 'items-center'
+            return 'center'
         }
         else {
-            return 'items-start'
+            return 'start'
         }
     }
 
     let location = useLocation();
     const getActive = (path) => {
         if(location.pathname === path) {
-            return 'text-red hover:text-red-dark'
+            return 'red'
         }
         else{
-            return 'text-white hover:text-gray_dark'
+            return 'white'
         }
     }
     const handleLogout = () => {
@@ -51,54 +86,54 @@ const Navbar = () => {
     }
 
     return (
-        <Container className='fixed hidden md:flex bg-black h-screen transition-all z-40' width={width}>
-            <div className='absolute top-4 -right-4 w-12 h-12 bg-red rounded-full flex justify-center items-center'>
+        <Container width={width}>
+            <Icon >
                 <Hamburger size={20} color="#fff" onToggle={handleNav}/>
-            </div>
-            <div className={`flex flex-col justify-center px-4 ${getJustify} w-full gap-y-8`}>
+            </Icon>
+            <Wrapper align = {`${getJustify}`}>
                 <Link to='/'>
-                    <div className={`flex gap-x-4 cursor-pointer ${getActive('/overview')}`}>
+                    <div color={`${getActive("/overview")}`}>
                         <RiDashboardFill className='w-6 h-6'/>
-                        { isOpen && <h3>Overview</h3>}
+                        { isOpen && <h3>Overview</h3>}  
                     </div>
                 </Link>
                 <Link to='/requests'>
-                    <div className={`flex gap-x-4 cursor-pointer ${getActive('/team')}`}>
+                    <div color={`${getActive("/team")}`}>
                         <IoMdInformationCircle className='w-6 h-6'/>
                         { isOpen && <h3>Teams</h3>}
                     </div>
                 </Link>
                 <Link to='/messages'>
-                    <div className={`flex gap-x-4 cursor-pointer ${getActive('/expenses')}`}>
+                    <div color={`${getActive("/exxpenses")}`}>
                         <AiFillMessage className='w-6 h-6'/>
                         { isOpen && <h3>Expenses</h3>}
                     </div>
                 </Link>
                 <Link to='/partner'>
-                    <div className={`flex gap-x-4 cursor-pointer ${getActive('/activity')}`}>
+                    <div color={`${getActive("/activity")}`}>
                         <MdPersonAddAlt1 className='w-6 h-6'/>
                         { isOpen && <h3>Activity</h3>}
                     </div>
                 </Link>
                 <Link to='/setting'>
-                    <div className={`flex gap-x-4 cursor-pointer ${getActive('/inventory')}`}>
+                    <div color={`${getActive("/inventory")}`}>
                         <IoMdSettings className='w-6 h-6'/>
                         { isOpen && <h3>Inventory</h3>}
                     </div>
                 </Link>
                 <Link to='/gallery'>
-                    <div className={`flex gap-x-4 cursor-pointer ${getActive('/setting')}`}>
+                    <div color={`${getActive("/seetting")}`}>
                         <RiGalleryFill className='w-6 h-6'/>
                         { isOpen && <h3>Setting</h3>}
                     </div>
                 </Link>
-                <div className='flex gap-x-4 cursor-pointer text-white' onClick={handleLogout}>
+                <div color={`${getActive("/overview")}`} onClick={handleLogout}>
                     <AiOutlineLogout className='w-6 h-6'/>
                     { isOpen && <h3>Logout</h3>}
                 </div>
-            </div>        
+            </Wrapper>          
         </Container>
     )
 }
 
-export default Navbar
+export default Navbar;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './home.css';
-
+import Navbar from '../../Components/Organisms/sidebar';
 const Home = () => {
   return (
 
     <div className='container'>
+      <Navbar/>
+      <div className="dev">
       <h1>Merak | Home</h1>
       <Link to="/inventory" style={{textDecoration: 'none', color: 'gray'}}><h3>Go to Inventory</h3></Link>
       <Link to="/order" style={{textDecoration: 'none', color: 'gray'}}><h3>Go to Order</h3></Link>
+      </div>
 
     </div>
   )
