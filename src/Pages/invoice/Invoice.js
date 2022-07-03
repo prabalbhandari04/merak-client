5df1472cc61ddcf09df16e06c4ce6eef6d9df579
3ndless_____ <57608074+3ndre@users.noreply.github.com>
2022-07-03 20:15:11 +0545

    Invoice status and table update

--- src/Pages/invoice/Invoice.js
+++ src/Pages/invoice/Invoice.js
@@ -1,149 +1,68 @@
-import { useState } from 'react';
-import {useSelector} from 'react-redux';
+import { useState, useEffect } from 'react';
+//Redux
+import {useSelector, useDispatch} from 'react-redux';
+import {loadOrders} from '../../redux/actions/ordersActions';
 
 
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { Link as RouterLink} from 'react-router-dom';
 // @mui
 import { useTheme } from '@mui/material/styles';
 import {
   Box,
-  Tab,
-  Tabs,
   Card,
   Table,
   Stack,
-  Switch,
-  Button,
-  Tooltip,
   Divider,
   TableBody,
   Container,
-  IconButton,
   TableContainer,
   TablePagination,
-  FormControlLabel,
   Typography,
+  TableRow, TableCell, TableHead,  
 } from '@mui/material';
 
-// hooks
-import useTabs from '../../hooks/useTabs';
+
 import useSettings from '../../hooks/useSettings';
-import useTable, { getComparator, emptyRows } from '../../hooks/useTable';
+import useTable, {  emptyRows } from '../../hooks/useTable';
 
 // components
 import Page from '../../components/Page';
 import Label from '../../components/Label';
-import Iconify from '../../components/Iconify';
 import Scrollbar from '../../components/Scrollbar';
-import { TableEmptyRows, TableHeadCustom, TableNoData, TableSelectedActions } from '../../components/table';
+import { TableEmptyRows } from '../../components/table';
 // sections
 import InvoiceAnalytic from './InvoiceAnalytic';
-import { InvoiceTableRow } from '../invoice/list';
 
 // ----------------------------------------------------------------------
 
 
-const TABLE_HEAD = [
-  { id: 'invoiceNumber', label: 'Invoice', align: 'left' },
-  { id: 'createDate', label: 'Ordered by', align: 'left' },
-  { id: 'dueDate', label: 'Assigned to', align: 'left' },
-  { id: 'date', label: 'Ordered date', align: 'lfet'},
-  { id: 'status', label: 'Status', align: 'left' },
-  { id: '' },
-];
-
-// ----------------------------------------------------------------------
 
 export default function Invoice() {
   const theme = useTheme();
+
+  const dispatch = useDispatch();
 
   const { themeStretch } = useSettings();
 
 
   const {orders} = useSelector(state => state.data1); //Redux State
+
+
+  //Fetching All Products - loadProducts le redux ko -> Action ma (dispatch gareko) Api call gareko cha (GET)
+  useEffect(() => {
+    dispatch(loadOrders());
+  }, [dispatch]);
   
 
   const {
     dense,
     page,
-    order,
-    orderBy,
     rowsPerPage,
-    setPage,
-    //
-    selected,
-    setSelected,
-    onSelectRow,
-    onSelectAllRows,
-    //
-    onSort,
-    onChangeDense,
     onChangePage,
     onChangeRowsPerPage,
   } = useTable({ defaultOrderBy: 'createDate' });
 
-  const [tableData, setTableData] = useState([]);
-
-  const [filterName, setFilterName] = useState('');
-
-  const [filterService, setFilterService] = useState('all');
-
-  const [filterStartDate, setFilterStartDate] = useState(null);
-
-  const [filterEndDate, setFilterEndDate] = useState(null);
-
-  const { currentTab: filterStatus, onChangeTab: onFilterStatus } = useTabs('all');
-
-  const handleFilterName = (filterName) => {
-    setFilterName(filterName);
-    setPage(0);
-  };
-
-  const handleFilterService = (event) => {
-    setFilterService(event.target.value);
-  };
-
-  const handleDeleteRow = (id) => {
-    const deleteRow = tableData.filter((row) => row.id !== id);
-    setSelected([]);
-    setTableData(deleteRow);
-  };
-
-  const handleDeleteRows = (selected) => {
-    const deleteRows = tableData.filter((row) => !selected.includes(row.id));
-    setSelected([]);
-    setTableData(deleteRows);
-  };
-
-  const handleEditRow = (id) => {
-    
-  };
-
-  const handleViewRow = (id) => {
-    
-  };
-
-  const dataFiltered = applySortFilter({
-    tableData,
-    comparator: getComparator(order, orderBy),
-    filterName,
-    filterService,
-    filterStatus,
-    filterStartDate,
-    filterEndDate,
-  });
-
-  const denseHeight = dense ? 56 : 76;
-
-  const isNotFound =
-    (!dataFiltered.length && !!filterName) ||
-    (!dataFiltered.length && !!filterStatus) ||
-    (!dataFiltered.length && !!filterService) ||
-    (!dataFiltered.length && !!filterEndDate) ||
-    (!dataFiltered.length && !!filterStartDate);
-
-  const getLengthByStatus = (status) => tableData.filter((item) => item.status === status).length;
-
+  const [tableData, ] = useState([]);
 
 
 
@@ -165,13 +84,6 @@ export default function Invoice() {
 
 
 
-  
-  const TABS = [
-    { value: 'all', label: 'All', color: 'info', count: total },
-    { value: 'completed', label: 'Completed', color: 'success', count: completed},
-    { value: 'pending', label: 'Pending', color: 'warning', count: pending},
-    { value: 'cancelled', label: 'Cancelled', color: 'error', count: cancelled},
-  ];
 
 
 
@@ -230,27 +142,7 @@ export default function Invoice() {
         </Card>
 
         <Card>
-          <Tabs
-            allowScrollButtonsMobile
-            variant="scrollable"
-            scrollButtons="auto"
-            value={filterStatus}
-            onChange={onFilterStatus}
-            sx={{ px: 2, bgcolor: 'background.neutral' }}
-          >
-            {TABS.map((tab) => (
-              <Tab
-                disableRipple
-                key={tab.value}
-                value={tab.value}
-                label={
-                  <Stack spacing={1} direction="row" alignItems="center">
-                    <div>{tab.label}</div> <Label color={tab.color}> {tab.count} </Label>
-                  </Stack>
-                }
-              />
-            ))}
-          </Tabs>
+         
 
           <Divider />
 
@@ -258,79 +150,87 @@ export default function Invoice() {
 
           <Scrollbar>
             <TableContainer sx={{ minWidth: 800, position: 'relative', mt: '13px'}}>
-              {selected.length > 0 && (
-                <TableSelectedActions
-                  dense={dense}
-                  numSelected={selected.length}
-                  rowCount={tableData.length}
-                  onSelectAllRows={(checked) =>
-                    onSelectAllRows(
-                      checked,
-                      tableData.map((row) => row.id)
-                    )
-                  }
-                  actions={
-                    <Stack spacing={1} direction="row">
-                      <Tooltip title="Sent">
-                        <IconButton color="primary">
-                          <Iconify icon={'ic:round-send'} />
-                        </IconButton>
-                      </Tooltip>
-
-                      <Tooltip title="Download">
-                        <IconButton color="primary">
-                          <Iconify icon={'eva:download-outline'} />
-                        </IconButton>
-                      </Tooltip>
-
-                      <Tooltip title="Print">
-                        <IconButton color="primary">
-                          <Iconify icon={'eva:printer-fill'} />
-                        </IconButton>
-                      </Tooltip>
-
-                      <Tooltip title="Delete">
-                        <IconButton color="primary" onClick={() => handleDeleteRows(selected)}>
-                          <Iconify icon={'eva:trash-2-outline'} />
-                        </IconButton>
-                      </Tooltip>
-                    </Stack>
-                  }
-                />
-              )}
+             
 
               <Table size={dense ? 'small' : 'medium'}>
-                <TableHeadCustom
-                  order={order}
-                  orderBy={orderBy}
-                  headLabel={TABLE_HEAD}
-                  rowCount={tableData.length}
-                  numSelected={selected.length}
-                  onSort={onSort}
-                  onSelectAllRows={(checked) =>
-                    onSelectAllRows(
-                      checked,
-                      tableData.map((row) => row.id)
-                    )
-                  }
-                />
+               
+              <TableHead>
+              
+                    <TableRow>
+                        <TableCell>
+                          Invoice
+                        </TableCell>
 
-                <TableBody>
-                  {dataFiltered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-                    <InvoiceTableRow
-                      key={row.id}
-                      row={row}
-                      selected={selected.includes(row.id)}
-                      onSelectRow={() => onSelectRow(row.id)}
-                      onViewRow={() => handleViewRow(row.id)}
-                      onEditRow={() => handleEditRow(row.id)}
-                      onDeleteRow={() => handleDeleteRow(row.id)}
-                    />
-                  ))}
+                        <TableCell>
+                          Ordered by
+                        </TableCell>
 
-                  <TableEmptyRows height={denseHeight} emptyRows={emptyRows(page, rowsPerPage, tableData.length)} />
+                        <TableCell>
+                         Assigned to
+                        </TableCell>
 
-                  <TableNoData isNotFound={isNotFound} />
+                        <TableCell>
+                         Ordered date
+                        </TableCell>
+
+                        <TableCell>
+                          Status
+                        </TableCell>
+     
+                    </TableRow>
+
+                   
+
+                  </TableHead>
+
+                      <TableBody>
+
+                      {orders && orders.map((orders) => {
+                
+                        return (
+                            <TableRow hover >
+                              
+
+                              <TableCell sx={{ display: 'flex', alignItems: 'center' }}>
+
+                                <Stack>
+                                  <Typography variant="subtitle2" noWrap>
+                                    {orders.invoice}
+                                  </Typography>
+                                </Stack>
+                              </TableCell>
+
+                              <TableCell align="left">{orders.ordered_by === null ? null : orders.ordered_by.name}</TableCell>
+
+                              <TableCell align="left">{orders.assigned_to === null ? null : orders.assigned_to.full_name}</TableCell>
+
+                              <TableCell align="left">{orders.ordered_date === null ? null : orders.ordered_date}</TableCell>
+
+                              
+
+                              <TableCell align="left">
+                                <Label
+                                  variant={theme.palette.mode === 'light' ? 'ghost' : 'filled'}
+                                  color={
+                                    (orders.status === 'COMPLETED' && 'success') ||
+                                    (orders.status === 'PENDING' && 'warning') ||
+                                    (orders.status === 'CANCELLED' && 'error') ||
+                                    'default'
+                                  }
+                                  sx={{ textTransform: 'capitalize' }}
+                                >
+                                  {orders.status}
+                                </Label>
+                              </TableCell>
+
+                              
+                            </TableRow>
+                              )
+                             })}
+
+                  <TableEmptyRows emptyRows={emptyRows(page, rowsPerPage, tableData.length)} />
+
+               
                 </TableBody>
               </Table>
             </TableContainer>
@@ -340,7 +240,7 @@ export default function Invoice() {
             <TablePagination
               rowsPerPageOptions={[5, 10, 25]}
               component="div"
-              count={dataFiltered.length}
+              count={total}
               rowsPerPage={rowsPerPage}
               page={page}
               onPageChange={onChangePage}
@@ -355,49 +255,3 @@ export default function Invoice() {
   );
 }
 
-// ----------------------------------------------------------------------
-
-function applySortFilter({
-  tableData,
-  comparator,
-  filterName,
-  filterStatus,
-  filterService,
-  filterStartDate,
-  filterEndDate,
-}) {
-  const stabilizedThis = tableData.map((el, index) => [el, index]);
-
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-
-  tableData = stabilizedThis.map((el) => el[0]);
-
-  if (filterName) {
-    tableData = tableData.filter(
-      (item) =>
-        item.invoiceNumber.toLowerCase().indexOf(filterName.toLowerCase()) !== -1 ||
-        item.invoiceTo.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
-    );
-  }
-
-  if (filterStatus !== 'all') {
-    tableData = tableData.filter((item) => item.status === filterStatus);
-  }
-
-  if (filterService !== 'all') {
-    tableData = tableData.filter((item) => item.items.some((c) => c.service === filterService));
-  }
-
-  if (filterStartDate && filterEndDate) {
-    tableData = tableData.filter(
-      (item) =>
-        item.createDate.getTime() >= filterStartDate.getTime() && item.createDate.getTime() <= filterEndDate.getTime()
-    );
-  }
-
-  return tableData;
-}
